dce4b0620447eca0e277e07fd78402d8477d7e8e
Brubzick <blubczz@gmail.com>
2024-09-20 14:16:28 +0800

    加入了asm的部分

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,24 @@
 #coding:utf-8
 from func import get_func_cfgs_c
 from func_disasm import get_func_cfgs_disasm
+from func_asm import get_func_cfgs_asm
 import os
 import pickle
 import argparse
+import subprocess
+
+ASM_PARSER_PATH = '../asm-parser/build/bin/asm-parser' # asm-parser的路径
+ASM_PARSER_OUT_DIR = ''
 
 if __name__ == '__main__':
 	# 输入参数
 	parser = argparse.ArgumentParser(description='input parameter')
 	parser.add_argument('--input', '-i', type=str, required=True, help='input file path') # 输入的路径
 	parser.add_argument('--output', '-o', type=str, help='output file path, the same as the input dir by default') # 保存ACFG的文件夹的路径
-	parser.add_argument('--type', '-t', type=str, required=True, help='input type, \'binary\' or \'disassembly\'') # 类型
+	parser.add_argument('--type', '-t', type=str, required=True, help='input type, \'bin\' or \'disasm\' or \'asm\'') # 类型
 	parser.add_argument('--arch', '-a', type=str, help='arch of input, \'x86\' or \'arm\', only required if input type is disassembly') # 汇编码的架构
+	parser.add_argument('--parse', '-p', type=bool, help='whether use asm-parser, default True') # 是否使用asm-parser
+
 	args = parser.parse_args()
 
 	filePath = args.input # 输入的路径
@@ -21,17 +28,17 @@ if __name__ == '__main__':
 	if path == None:
 		path = os.path.dirname(filePath)
 
-	inputType = args.type # 'binary'或'disassembly', 输入为二进制或汇编码文本
+	inputType = args.type # 'bin'或'disasm'或'asm', 输入为二进制或汇编码文本
 
 	haveOutput = False # 是否成功输出
 
 	# 二进制，支持x86和arm
-	if inputType == 'binary':
+	if inputType == 'bin':
 		cfgs = get_func_cfgs_c(filePath, fileName)
 		haveOutput = True
 	
-	# 汇编码，采用Binary Ninja的文本排列格式。
-	elif inputType == 'disassembly':
+	# 汇编码，采用Binary Ninja的文本排列格式(即反汇编的结果)。
+	elif inputType == 'disasm':
 		arch = args.arch # 支持x86和arm
 		if arch == None:
 			print('The arch[--arch/-a] is required for disassembly. \'x86\' and \'arm\' are supported.')
@@ -40,6 +47,20 @@ if __name__ == '__main__':
 		else:
 			cfgs = get_func_cfgs_disasm(filePath, fileName, arch)
 			haveOutput = True
+	# 汇编码，gcc或clang的汇编结果
+	elif inputType == 'asm':
+		parse = args.parse
+		if parse == None:
+			parse = True
+		if parse:
+			parser_output = os.path.join(ASM_PARSER_OUT_DIR, fileName+'.txt')
+			command = './' + ASM_PARSER_PATH + ' ' + filePath + ' -unused_labels -directives -comment_only  -outputtext > ' + parser_output
+			process = subprocess.run(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+			filePath = parser_output
+		cfgs = get_func_cfgs_asm(filePath, fileName)
+		haveOutput = True
+
+		
 	else:
 		print('Wrong type. Use --help to see the parameters.')
 
